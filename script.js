19d53fb24581999c78ead214b64d6268ca995836
ehunck <ehunck@aol.com>
2023-12-14 22:41:09 -0600

    - sort of working

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let reader;
     let writer;
     let keepReading = false;
+    let pendingData = '';
+    let awaitingHelpResponse = false;
+
 
     async function connectSerial() {
         try {
@@ -47,46 +50,79 @@ document.addEventListener('DOMContentLoaded', () => {
         sendCommandButton.disabled = true;
     }
 
-    async function sendCommand() {
+    async function sendCommand(command) {
         if (writer) {
+            lastCommandSent = command; // Store the last command sent
+            await writer.write(new TextEncoder().encode(command + "\r\n"));
+        }
+    }
+
+    async function sendHelpCommand() {
+        if (writer) {
+            buttonsDiv.innerHTML = ''; // Clear the buttonsDiv when the help command is sent
             command = "echo off\r\n";
             await writer.write(new TextEncoder().encode(command));
             command = "help\r\n";
             await writer.write(new TextEncoder().encode(command));
+            awaitingHelpResponse = true;
+            pendingData = ''; // Reset pending data
+            lastCommandSent = '';
         }
     }
 
-    function processReceivedData(data) {
-        buttonsDiv.innerHTML = ''; // Clear existing buttons
-        const lines = data.split('\n');
-        lines.forEach(line => {
-            const [label, ...noteParts] = line.trim().split(' ');
-            if (label) {
-                const note = noteParts.join(' ');
-                const buttonWrapper = document.createElement('div'); // Create a wrapper div for each button
-                const button = document.createElement('button');
-                button.textContent = label;
-                button.title = note; // Set the tooltip
-                buttonWrapper.appendChild(button); // Append the button to the wrapper
-                buttonsDiv.appendChild(buttonWrapper); // Append the wrapper to the buttonsDiv
-            }
-        });
+    function createButtonWithResponse(command) {
+        const buttonWrapper = document.createElement('div');
+        const button = document.createElement('button');
+        button.textContent = command;
+        buttonWrapper.appendChild(button);
+
+        const responseDiv = document.createElement('div');
+        responseDiv.id = `response-${command}`;
+        buttonWrapper.appendChild(responseDiv);
+
+        button.addEventListener('click', () => sendCommand(command));
+
+        return buttonWrapper;
+    }
+
+    function updateResponseDisplay(command, response) {
+        const responseDiv = document.getElementById(`response-${command}`);
+        if (responseDiv) {
+            responseDiv.textContent = response;
+        }
     }
 
     async function readSerial() {
         const decoder = new TextDecoder();
-        let receivedData = '';
 
         while (port.readable && keepReading) {
             try {
                 const { value, done } = await reader.read();
                 if (done) {
-                    // Reader has been canceled.
-                    break;
+                    break; // Reader has been canceled
                 }
                 const text = decoder.decode(value, {stream: true});
-                receivedData += text;
-                processReceivedData(receivedData);
+                pendingData += text;
+
+                // Process complete lines
+                let eolIndex;
+                while ((eolIndex = pendingData.indexOf('\n')) >= 0) {
+                    let line = pendingData.slice(0, eolIndex).trim();
+                    pendingData = pendingData.slice(eolIndex + 1);
+
+                    if (awaitingHelpResponse) {
+                        if (line.toLowerCase().startsWith("commands:")) {
+                            // Start processing commands after this line
+                            awaitingHelpResponse = false;
+                        }
+                    } else if( lastCommandSent === "" ) {
+                        processLine(line); // Process the line as a command
+                    }
+                    else{
+                        processReceivedData(line); // Process the line as a potential response
+                    }
+                    
+                }
             } catch (error) {
                 console.error('Read error:', error);
                 break;
@@ -94,7 +130,46 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    function processReceivedData(line) {
+        if (line && lastCommandSent) {
+            // Assuming the line is a response to the last command sent
+            updateResponseDisplay(lastCommandSent, line);
+            lastCommandSent = ''; // Reset the last command sent
+        }
+    }
+
+    function processLine(line) {
+        const [command, ...noteParts] = line.split(' ');
+        if (command) {
+            createButtonWithResponse(command, noteParts.join(' '));
+        }
+    }
+
+    function processHelpLine(line) {
+        const [command, ...noteParts] = line.split(' ');
+        if (command) {
+            createButtonWithResponse(command, noteParts.join(' '));
+        }
+    }
+
+    function createButtonWithResponse(command, note) {
+        const buttonWrapper = document.createElement('div');
+        const button = document.createElement('button');
+        button.textContent = command;
+        button.title = note; // Set the tooltip
+        buttonWrapper.appendChild(button);
+
+        const responseDiv = document.createElement('div');
+        responseDiv.id = `response-${command}`;
+        buttonWrapper.appendChild(responseDiv);
+
+        button.addEventListener('click', () => sendCommand(command));
+
+        buttonsDiv.appendChild(buttonWrapper);
+    }
+
+
     connectButton.addEventListener('click', connectSerial);
     disconnectButton.addEventListener('click', disconnectSerial);
-    sendCommandButton.addEventListener('click', sendCommand);
+    sendCommandButton.addEventListener('click', sendHelpCommand);
 });
